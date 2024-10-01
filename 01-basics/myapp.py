95e3fd7aafc3e13b2960054902247e99ad122df6
Balner123 <stepan.balner@proton.me>
2024-10-01 16:28:29 +0200

    firstB1

---
+++ 01-basics/myapp.py
@@ -0,0 +1,9 @@
+"""
+Cvičení 3:
+
+Vytvořte originální aplikaci v Pythonu, v níž požádáte uživatele o různé vstupní údaje a
+využijete na maximum možností výstupů do konzole. Může to být vtipný dotazník, jednoduchý znalostní test (např.
+z matematiky...), průvodce fiktivní instalací atd. Fantazii se meze nekladou a vtipnější vyhrává :-)
+Aplikaci uložte do samostatného souboru myapp.py.
+"""
+
---
+++ README.md
@@ -0,0 +1,2 @@
+"# projekt-python" 
+# PYTHON-PROJEKT
